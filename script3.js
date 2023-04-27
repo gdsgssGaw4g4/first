220ff5c88639e6c725240caff733406459e1d79b
gdsgssGaw4g4 <131213282+gdsgssGaw4g4@users.noreply.github.com>
2023-04-27 20:38:38 +0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -2,19 +2,10 @@
 var link = document.querySelector('#elCopyright a');
 
 // Change the text of the link element to "Powered by Panolas"
-link.innerHTML = 'Powered by Panolas';
+link.innerHTML = 'Powered by Panolas and Invision Community';
 
 // Change the title attribute of the link element
 link.title = 'Powered by Panolas';
 
 // Set the link's href attribute to the YouTube URL
-link.href = 'https://ultracheats.net/';
-
-// Add a click event listener to the link
-link.addEventListener('click', function(event) {
-  // Prevent the default link behavior
-  event.preventDefault();
-  
-  // Set the location to the https://ultracheats.net/ URL
-  location.href = 'https://ultracheats.net/';
-});
+link.href = 'https://invisioncommunity.com/';
