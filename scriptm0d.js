dc500b29b0d8b77af36cbf819695540a3d87f682
gdsgssGaw4g4 <131213282+gdsgssGaw4g4@users.noreply.github.com>
2023-05-28 19:37:20 +0300

    Update and rename script3.js to scriptm0d.js

---
+++ scriptm0d.js
@@ -0,0 +1,11 @@
+// Get the link element
+var link = document.querySelector('#elCopyright a');
+
+// Change the text of the link element to "Copyright ©2023 Modycore All Rights Reserved."
+link.innerHTML = 'Copyright ©2023 Modycore All Rights Reserved.';
+
+// Change the title attribute of the link element
+link.title = 'Modycore';
+
+// Set the link's href attribute to the YouTube URL
+link.href = 'https://www.modycore.com/';
