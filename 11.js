d477e4560019cc071d4d4ae08328cbc6dcd4e624
gdsgssGaw4g4 <131213282+gdsgssGaw4g4@users.noreply.github.com>
2023-11-26 01:30:40 +0200

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    var element = document.querySelector('.watermark');
+    if (element) {
+        element.style.display = 'none !important';
+    }
+});
